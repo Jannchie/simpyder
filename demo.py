b24d141fdd0ee4b55af4fe2ef37e7e3f27951d53
jannchie <jannchie@gmail.com>
2020-01-17 01:20:51 +0900

    UPDATE: Demo

--- demo.py
+++ demo.py
@@ -1,30 +1,43 @@
-from pyspider import Spider
+from spipy import Spider
 import requests
 from lxml.etree import HTML
-headers = {
-    'user-agent': 'Mozilla/5.0 (Windows NT 10.0 Win64 x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36'}
 
 
-class Demo(Spider):
-    def __init__(self):
-        super().__init__()
-        self.NAME = "demo"
+# class Demo(Spider):
+#     def __init__(self):
+#         super().__init__()
+#         self.NAME = "demo"
+#     pass
+
+#     def gen_url(self):
+#         for each_id in range(100):
+#             yield "https://www.bilibili.com/video/av{}".format(each_id)
+
+#     def parse(self, response, key=None):
+#         return response
+
+#     def save(self, item):
+#         pass
+#         print(HTML(item.text).xpath('//meta[@name="title"]/@content')[0])
+
+
+def gen_url():
+    for each_id in range(100):
+        yield "https://www.bilibili.com/video/av{}".format(each_id)
+
+
+def parse(response, key=None):
+    return response
+
+
+def save(item):
     pass
-
-    def get_key(self):
-        for each_id in range(100):
-            self.item_queue.put(each_id)
-
-    def get_response(self, key):
-        response = requests.get(
-            "https://www.bilibili.com/video/av{}".format(key), headers=headers)
-        return response
-
-    def parse(self, response, key=None):
-        return response
-
-    def save(self, item):
-        print(HTML(item.text).xpath('//meta[@name="title"]/@content')[0])
+    print(HTML(item.text).xpath('//meta[@name="title"]/@content')[0])
 
 
-Demo()
+# s = Spider()
+s = Spider(gen_url, parse, save)
+# s.assemble(gen_url, parse, save)
+s.run()
+
+# Demo()
