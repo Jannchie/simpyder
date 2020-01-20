231f954fc2c4bbacedbae41e00659c36a0527788
jannchie <jannchie@gmail.com>
2020-01-20 23:24:33 +0900

    UPDATE: .travis.yml

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.4',
     install_requires=[
         'requests==2.22.0',
         'lxml==4.3.4',
