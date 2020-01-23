e5c21af893f9abf2848a8ca535e3b03b40912a66
jannchie <jannchie@gmail.com>
2020-01-23 21:49:04 +0900

    UPDATE: demo

--- demo/demo.py
+++ demo/demo.py
@@ -1,6 +1,11 @@
 # coding=utf-8
+'''
+这是一个DEMO。该程序用于爬取B站AV号小于100的视频页面标题
+'''
 
-from simpyder.spiders import Spider
+import requests
+from simpyder import Spider
+from simpyder import SimpyderConfig
 
 
 def gen_url():
@@ -13,13 +18,13 @@ def parse(response):
 
 
 def save(item):
-    # print(item)
+    print(item)
     pass
 
 
 s1 = Spider(gen_url, parse, save, name="DEMO")
+sc = SimpyderConfig
+sc.COOKIES = "example:value"
+sc.USER_AGENT = "my user agent"
+s1.assemble(gen_url=gen_url, parse=parse, save=save, config=sc)
 s1.run()
-
-# s2 = Spider(name="DEMO")
-# s2.assemble(gen_url, parse, save)
-# s2.run()
