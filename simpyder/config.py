5e149fb3e92f84648c82a3c2ecd4d3a0e0aa23e8
jannchie <jannchie@gmail.com>
2020-01-23 22:24:28 +0900

    UPDATE config

--- simpyder/config.py
+++ simpyder/config.py
@@ -3,7 +3,7 @@ from simpyder.utils import DEFAULT_UA
 
 class SimpyderConfig():
     USER_AGENT = DEFAULT_UA
-    COOKIES = ""
+    COOKIE = ""
     PARSE_THREAD_NUMER = 8
     HEADERS = None
     pass
