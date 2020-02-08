31732b4065200faa694199ab84ad02f94f3a33ac
jannchie <jannchie@gmail.com>
2020-02-08 16:36:35 +0900

    ADD: zhihu demo

--- demo/zhihu.py
+++ demo/zhihu.py
@@ -13,30 +13,30 @@ cookie = ''
 
 
 def get_url():
-    while True:
-        yield 'https://www.zhihu.com/hot'
-        sleep(60)
+  while True:
+    yield 'https://www.zhihu.com/hot'
+    sleep(60)
 
 
 def parse(response):
-    hot_list = response.xpath('//div[@class="HotItem-content"]')
-    data = []
-    for hot_item in hot_list:
-        try:
-            title = hot_item.xpath('.//h2[@class="HotItem-title"]/text()')[0]
-            print(title)
-            point = re.findall(r"\d+\.?\d*", hot_item.xpath(
-                './/div[contains(@class,"HotItem-metrics")]/text()')[0])[0]
+  hot_list = response.xpath('//div[@class="HotItem-content"]')
+  data = []
+  for hot_item in hot_list:
+    try:
+      title = hot_item.xpath('.//h2[@class="HotItem-title"]/text()')[0]
+      print(title)
+      point = re.findall(r"\d+\.?\d*", hot_item.xpath(
+          './/div[contains(@class,"HotItem-metrics")]/text()')[0])[0]
 
-            print(point)
-            data.append({
-                'date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
-                'title': title,
-                'value': int(point)
-            })
-        except Exception as e:
-            print('[ERROR]'.format(title))
-    return data
+      print(point)
+      data.append({
+          'date': datetime.datetime.now().strftime("%Y-%m-%d %H:%M"),
+          'title': title,
+          'value': int(point)
+      })
+    except Exception as e:
+      print('[ERROR]'.format(title))
+  return data
 
 
 f = csv.DictWriter(open('./zhihu.csv', 'w', encoding='utf-8-sig'),
@@ -44,9 +44,9 @@ f = csv.DictWriter(open('./zhihu.csv', 'w', encoding='utf-8-sig'),
 
 
 def save(items):
-    for item in items:
-        f.writerow(item)
-    pass
+  for item in items:
+    f.writerow(item)
+  pass
 
 
 s = Spider()
